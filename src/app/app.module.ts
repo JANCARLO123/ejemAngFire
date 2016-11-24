a0310494f12fc5c7c66fbd752107919d91625d87
JanCarlo <xmen_vallejo@hotmail.com>
2016-11-23 20:28:25 -0600

    Agregar FIrebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCIlh40cQPvfBIbUwxIqOyd-vkmsrDmeF4',
+  authDomain: 'angular-firebase-e6510.firebaseapp.com',
+  databaseURL: 'https://angular-firebase-e6510.firebaseio.com',
+  storageBucket: 'angular-firebase-e6510.appspot.com',
+  messagingSenderId: '250459244524'
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +21,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
